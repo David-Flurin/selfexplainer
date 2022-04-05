99ff8d904f7c48f23d11e47f0ceb457488886925
David <davidn@ethz.ch>
2022-04-05 17:46:00 +0200

    mergefix

--- src/utils/weighting.py
+++ src/utils/weighting.py
@@ -10,11 +10,7 @@ def softmax_weighting(t, k):
     s = h*w
     for i in range(batch_size):
         for c in range(num_classes):
-<<<<<<< HEAD
-            weights = torch.softmax(t[i, c].flatten() * k, 0)
-=======
             weights = torch.softmax(t[i, c].view(s) * k, 0)
->>>>>>> fd74c1f0263350a744c2342bd6a5f52b6c7ab301
             t[i, c] = torch.reshape(weights, (h, w))
     return  t
 
