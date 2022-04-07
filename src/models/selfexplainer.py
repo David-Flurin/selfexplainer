bugfix weighting

src/models/selfexplainer.py
@@ -81,9 +81,10 @@ class SelfExplainer(pl.LightningModule):
             segmentations = self.model(image) # [batch_size, num_classes, height, width]
         target_mask, non_target_mask = extract_masks(segmentations, targets, gpu=self.gpu) # [batch_size, height, width]
         
-        #weighted_segmentations = softmax_weighting(segmentations, self.weighting_koeff)
-        #logits = weighted_segmentations.sum(dim=(2,3))
-        logits = segmentations.mean((2,3))
+        weighted_segmentations = softmax_weighting(segmentations, self.weighting_koeff)
+        logits = weighted_segmentations.sum(dim=(2,3))
+        #logits_mean = segmentations.mean((2,3))
+        
         
         return segmentations, target_mask, non_target_mask, logits
 

src/utils/weighting.py
@@ -10,6 +10,6 @@ def softmax_weighting(t, k):
     for i in range(batch_size):
         for c in range(num_classes):
             weights = torch.softmax(t[i, c].view(h*w) * k, 0)
-            t[i, c] = torch.reshape(weights, (h, w))
+            t[i, c] = torch.reshape(weights, (h, w)) * t[i, c]
     return  t
 