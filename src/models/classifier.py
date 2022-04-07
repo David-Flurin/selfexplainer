a9b95c6ee5e16d2126239b0c74104184e7d03fa4
David <davidn@ethz.ch>
2022-04-07 18:21:53 +0200

    toy dataset online generation

---
+++ src/models/classifier.py
@@ -0,0 +1,111 @@
+import torch
+import pytorch_lightning as pl
+
+from torch import nn, softmax
+from torch.optim import Adam
+from pathlib import Path
+
+from copy import deepcopy
+
+from torchvision import models
+from utils.helper import get_filename_from_annotations, get_targets_from_annotations, extract_masks, Distribution
+from utils.image_display import save_all_class_masks, save_mask, save_masked_image
+from utils.loss import TotalVariationConv, ClassMaskAreaLoss, entropy_loss, mask_similarity_loss
+from utils.metrics import MultiLabelMetrics, SingleLabelMetrics
+from utils.weighting import softmax_weighting
+
+class Classifier(pl.LightningModule):
+    def __init__(self, num_classes=20, dataset="VOC", learning_rate=1e-5, gpu=0, profiler=None, metrics_threshold=-1.0, save_path="./results/"):
+
+        super().__init__()
+
+        self.gpu = gpu
+        self.profiler = profiler
+
+        self.learning_rate = learning_rate
+
+        self.model = models.resnet50()
+        self.model.fc = nn.Linear(2048, num_classes)
+        self.dataset = dataset
+
+        self.setup_losses()
+        self.setup_metrics(num_classes=num_classes, metrics_threshold=metrics_threshold)
+
+        if self.dataset == 'TOY':
+            self.f_tex_dist = Distribution()
+            self.b_text_dist = Distribution()
+            self.shapes_dist = Distribution()
+
+
+    def setup_losses(self):
+        self.classification_loss_fn = nn.BCEWithLogitsLoss()
+
+    def setup_metrics(self, num_classes, metrics_threshold):
+        self.train_metrics = MultiLabelMetrics(num_classes=num_classes, threshold=metrics_threshold)
+        self.valid_metrics = MultiLabelMetrics(num_classes=num_classes, threshold=metrics_threshold)
+        self.test_metrics = MultiLabelMetrics(num_classes=num_classes, threshold=metrics_threshold)
+
+    
+
+
+    def forward(self, image, targets):
+        logits = self.model(image) 
+        return logits
+
+    def training_step(self, a, batch_idx):
+        image, annotations = a
+        targets = get_targets_from_annotations(annotations, dataset=self.dataset, gpu=self.gpu)
+
+        if self.dataset == 'TOY':
+            for a in annotations:
+                for obj in a['objects']:
+                    self.shapes_dist.update(obj[0])
+                    self.f_tex_dist.update(obj[1])
+            self.b_text_dist.update(a['background'])
+           
+        
+        logits = self(image, targets)
+        loss = self.classification_loss_fn(logits, targets)
+        self.log('loss', float(loss))
+       
+        self.train_metrics(logits, targets)
+        return loss
+
+    def training_epoch_end(self, outs):
+        self.log('train_metrics', self.train_metrics.compute())
+        self.train_metrics.reset()
+        self.f_tex_dist.print_distribution()
+        self.b_text_dist.print_distribution()
+        self.shapes_dist.print_distribution()
+
+    def validation_step(self, batch, batch_idx):
+        image, annotations = batch
+        targets = get_targets_from_annotations(annotations, dataset=self.dataset, gpu=self.gpu)
+        
+        
+        logits = self(image, targets)
+        loss = self.classification_loss_fn(logits, targets)
+
+        self.log('val_loss', loss)
+        self.valid_metrics(logits, targets) 
+   
+    def validation_epoch_end(self, outs):
+        self.log('val_metrics', self.valid_metrics.compute(), prog_bar=True)
+        self.valid_metrics.reset()
+
+    def test_step(self, batch, batch_idx):
+        image, annotations = batch
+        targets = get_targets_from_annotations(annotations, dataset=self.dataset, gpu=self.gpu)
+        
+        logits = self(image, targets)
+        loss = self.classification_loss_fn(logits)
+
+        self.log('test_loss', loss)
+        self.test_metrics(logits, targets)
+
+    def test_epoch_end(self, outs):
+        self.log('test_metrics', self.test_metrics.compute(), prog_bar=True)
+        self.test_metrics.reset()
+
+    def configure_optimizers(self):
+        return Adam(self.parameters(), lr=self.learning_rate)
---
+++ src/toy_dataset/generator.py
@@ -0,0 +1,257 @@
+from multiprocessing.sharedctypes import Value
+from os import mkdir
+from os.path import join
+import os
+import shutil
+import cv2
+from torch import _shape_as_tensor, randint, square
+from .shapes import *
+import matplotlib.pyplot as plt
+from matplotlib import colors
+from random import randint, sample
+from tqdm import tqdm
+from pathlib import Path
+import xml.etree.ElementTree as ET
+
+
+def get_i(idx, list):
+    return [list[i] for i in idx]
+
+class Generator:
+
+    shapes = [Circle, Triangle, Square, Pentagon, Hexagon, Octagon, Heart, Cross, Star]
+
+    base = 'dataset'
+    img_size = (224,224)
+    min_radius = 20
+    max_radius = 60
+
+
+    def __init__(self, f_textures, b_textures):
+
+        
+        with open(f_textures, 'r') as f:
+            f_textures_files = f.read().splitlines()
+        with open(b_textures, 'r') as f:
+            b_textures_files = f.read().splitlines()
+
+        base_dir = '/'.join(__file__.split('/')[0:-1])
+
+        self.f_texture_names = [s.split('.')[0].lower() for s in f_textures_files]
+        self.b_texture_names = [s.split('.')[0].lower() for s in b_textures_files]
+        self.shape_names = []
+        
+        self.f_textures=[]
+        for f in f_textures_files:
+            self.f_textures.append(plt.imread(join(base_dir, 'textures', f)))
+        
+        self.b_textures = []
+        for f in b_textures_files:
+            self.b_textures.append(plt.imread(join(base_dir, 'textures', f)))
+
+        self.num_f_tex = len(self.f_textures)
+        self.num_b_tex = len(self.b_textures)
+
+
+        
+    def generate_sample(self, num_objects):
+        if num_objects > len(self.shapes):
+            raise ValueError(f'Number of objects cannot exceed total available shapes ({len(self.shapes)})')
+        idx = sample(range(0, len(self.shapes)), num_objects)
+        return self.__generate(get_i(idx, self.shapes))
+
+    
+    def create(self, number_per_shape, proportions):
+
+        proportions = [float(x) for x in proportions]
+        if len(proportions) != 3 or sum(proportions) != 1.0:
+            raise ValueError('\'Proportions\' must have three elements (Train, Val, Test) and must sum to 1')
+
+
+        self.__directories()
+
+
+        samples_shape = {}
+        samples_texture = {}
+        with tqdm(total=number_per_shape*len(self.shapes)) as pbar:
+            num_length = math.ceil(math.log(number_per_shape*len(self.shapes), 10))+1
+            i = 1
+            for _ in range(number_per_shape):
+                for shape in self.shapes:
+                    filename = f'{i:0{num_length}d}'
+                    shapes, textures = self.generate([shape], filename)
+                    for s in shapes:
+                        try:
+                            samples_shape[s] += [filename]
+                        except KeyError:
+                            samples_shape[s] = [filename]
+                    for t in textures:
+                        try:
+                            samples_texture[self.f_texture_names[t]] += [filename]
+                        except KeyError:
+                            samples_texture [self.f_texture_names[t]] = [filename]
+                    
+                    pbar.update()
+                    i += 1
+        
+        
+
+        train = []
+        val = []
+        test = []
+        for k,v in samples_shape.items():
+            v.sort(key=int)
+            indices = range(0, len(v))
+            total = len(indices)
+            num_train = int(proportions[0] * total)
+            num_val = int(proportions[1] * total)
+            num_test = int(proportions[2] * total)
+            num_train += (total - num_train - num_val - num_test)
+            train_idx = sample(indices, num_train)
+            indices = [i for i in indices if i not in train_idx]
+            val_idx = sample(indices, num_val)
+            test_idx = [i for i in indices if i not in val_idx]
+
+            self.__write_sample_list(Path(self.base, 'imagesets', 'shapes', f'{k}_train.txt'), get_i(train_idx, v))
+            self.__write_sample_list(Path(self.base, 'imagesets', 'shapes', f'{k}_val.txt'), get_i(val_idx, v))
+            self.__write_sample_list(Path(self.base, 'imagesets', 'shapes', f'{k}_test.txt'), get_i(test_idx, v))
+            
+            
+            train += (get_i(train_idx, v))
+            val += (get_i(val_idx, v))
+            test += (get_i(test_idx, v))
+        
+        train.sort(key=int)
+        val.sort(key=int)
+        test.sort(key=int)
+        self.__write_sample_list(Path(self.base, 'imagesets', 'shapes', 'train.txt'), train)
+        self.__write_sample_list(Path(self.base, 'imagesets', 'shapes', 'val.txt'), val)
+        self.__write_sample_list(Path(self.base, 'imagesets', 'shapes', 'test.txt'), test)
+
+        train = []
+        val = []
+        test = []
+        for k,v in samples_texture.items():
+            v.sort(key=int)
+            indices = range(0, len(v))
+            total = len(indices)
+            num_train = int(proportions[0] * total)
+            num_val = int(proportions[1] * total)
+            num_test = int(proportions[2] * total)
+            num_train += (total - num_train - num_val - num_test)
+            train_idx = sample(indices, num_train)
+            indices = [i for i in indices if i not in train_idx]
+            val_idx = sample(indices, num_val)
+            test_idx = [i for i in indices if i not in val_idx]
+
+            self.__write_sample_list(Path(self.base, 'imagesets', 'textures', f'{k}_train.txt'), get_i(train_idx, v))
+            self.__write_sample_list(Path(self.base, 'imagesets', 'textures', f'{k}_val.txt'), get_i(val_idx, v))
+            self.__write_sample_list(Path(self.base, 'imagesets', 'textures', f'{k}_test.txt'), get_i(test_idx, v))
+            
+            
+            train += (get_i(train_idx, v))
+            val += (get_i(val_idx, v))
+            test += (get_i(test_idx, v))
+
+        train.sort(key=int)
+        
+
+                    
+    def __write_sample_list(self, filename, list):
+        with open(filename, 'w') as f:
+            for sample in list:
+                f.write(f'{sample}\n')
+
+    
+
+    def __generate(self, shapes):
+        shape_obj = []
+        for i, shape in enumerate(shapes):
+            y_part = self.img_size[1] // len(shapes)
+            radius, center_x, center_y, rotation, f_tex_idx, b_tex_idx = self.__randomize((self.img_size[0], y_part))
+            center_y += i*y_part
+            s = shape(radius, (center_x, center_y), self.img_size, rotation)
+            shape_obj.append((s, f_tex_idx))
+        img, seg = self.__assemble(shape_obj, self.b_textures[b_tex_idx])
+
+        objects = []
+        for shape in shape_obj:
+            shape_name = (str(shape[0].__class__).split('.')[-1]).split('\'')[0].lower()
+            objects.append((shape_name, self.f_texture_names[f_tex_idx]))
+        return {'image': img, 'segmentation': seg, 'objects': objects, 'background': self.b_texture_names[b_tex_idx]}
+
+
+    def __save(self, sample, filename):
+        
+        annotation = ET.Element('annotation')
+        xml_filename = ET.SubElement(annotation, 'filename')
+        xml_filename.text = filename
+        xml_objects = ET.SubElement(annotation, 'objects')
+        
+        for i, obj in enumerate(sample['objects']):
+            xml_obj = ET.SubElement(xml_objects, f'object{i}')
+            s = ET.SubElement(xml_obj, 'shape')
+            s.text = obj[0]
+            t = ET.SubElement(xml_obj, 'texture')
+            t.text = obj[1]
+        background = ET.SubElement(annotation, 'background')
+        background.text = sample['background']
+
+
+        cv2.imwrite(join(self.base, 'images', f'{filename}.jpg'), sample['image'])
+        cv2.imwrite(join(self.base, 'segmentations', f'{filename}.jpg'), sample['seg'])
+
+        tree = ET.ElementTree(annotation)
+        tree.write(join(self.base, 'annotations', f'{filename}.xml'))
+
+
+
+
+    def __randomize(self, size):
+        radius =  randint(self.min_radius, min((min(size)-30)/2, self.max_radius))
+        center_x = randint(20 + radius, size[0] - 20 - radius)
+        center_y = randint(20 + radius, size[1] - 20 - radius)
+        rotation = randint(0, 360)
+        f_tex_idx = randint(0, self.num_f_tex -1)
+        b_tex_idx = randint(0, self.num_b_tex -1)
+        return radius, center_x, center_y, rotation, f_tex_idx, b_tex_idx
+
+
+    def __directories(self):
+        if os.path.exists(self.base) and os.path.isdir(self.base):
+            shutil.rmtree(self.base)
+        mkdir(self.base)
+        mkdir(Path(self.base, 'images'))
+        mkdir(Path(self.base, 'segmentations'))
+        mkdir(Path(self.base, 'annotations'))
+        mkdir(Path(self.base, 'imagesets'))
+
+        mkdir(Path(self.base, 'imagesets', 'shapes'))
+        mkdir(Path(self.base, 'imagesets', 'textures'))
+        
+
+
+    def __assemble(self, shapes, b_tex):
+        b_tex = self.__crop_texture(b_tex, (224, 224))
+        img = b_tex.copy()
+        seg = np.zeros((self.img_size[0], self.img_size[1], 3))
+        for s, f_tex_idx in shapes:
+            f_tex = self.__crop_texture(self.f_textures[f_tex_idx], (224, 224))
+            img[np.where(s.mask)] = f_tex[np.where(s.mask)]
+            seg[np.where(s.mask)] = s.seg_color
+        return img, seg*255
+
+
+
+    def __crop_texture(self, tex, img_size):
+        t_h, t_w, _ = tex.shape
+        assert(t_h > img_size[0] and t_w > img_size[1])
+
+        x = randint(0, t_h - img_size[0] -1)
+        y = randint(0, t_w - img_size[1] -1)
+
+        cropped = tex[x:x+img_size[0], y:y+img_size[1]]
+        return cropped
+
+
+
