6763ca8883e8b90baeb7f01ccb18c7008d85f620
David <davidn@ethz.ch>
2022-04-25 11:08:55 +0200

    set perfect mask in config

--- src/models/selfexplainer.py
+++ src/models/selfexplainer.py
@@ -18,7 +18,8 @@ from matplotlib import pyplot as plt
 
 class SelfExplainer(pl.LightningModule):
     def __init__(self, num_classes=20, dataset="VOC", learning_rate=1e-5, weighting_koeff=1, pretrained=False, use_similarity_loss=False, use_entropy_loss=False, use_weighted_loss=False,
-    use_mask_area_loss=True, use_mask_variation_loss=True, mask_variation_regularizer=1.0, ncmask_total_area_regularizer=0.3, mask_area_constraint_regularizer=1.0, mask_total_area_regularizer=0.1, save_masked_images=False, save_masks=False, save_all_class_masks=False, gpu=0, profiler=None, metrics_threshold=-1.0, save_path="./results/"):
+    use_mask_area_loss=True, use_mask_variation_loss=True, mask_variation_regularizer=1.0, ncmask_total_area_regularizer=0.3, mask_area_constraint_regularizer=1.0, 
+    mask_total_area_regularizer=0.1, save_masked_images=False, use_perfect_mask=False, save_masks=False, save_all_class_masks=False, gpu=0, profiler=None, metrics_threshold=-1.0, save_path="./results/"):
 
         super().__init__()
 
@@ -56,7 +57,9 @@ class SelfExplainer(pl.LightningModule):
         self.setup_losses(dataset=dataset)
         self.setup_metrics(num_classes=num_classes, metrics_threshold=metrics_threshold)
 
+        #DEBUG
         self.i = 0.
+        self.use_perfect_mask = use_perfect_mask
 
     def setup_losses(self, dataset):
         if dataset == "CUB":
@@ -135,7 +138,10 @@ class SelfExplainer(pl.LightningModule):
             for _,p in self.frozen.named_parameters():
                 p.requires_grad_(False)
         
-        output = self(image, target_vector, torch.max(targets, dim=1)[0])
+        if self.use_perfect_mask:
+            output = self(image, target_vector, torch.max(targets, dim=1)[0])
+        else:
+            output = self(image, target_vector)
 
         if self.dataset == "CUB":
             labels = target_vector.argmax(dim=1)
