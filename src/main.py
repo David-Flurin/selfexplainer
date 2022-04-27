da245b0d788a4c577f7105d537ac2902ac7d7e3d
David <davidn@ethz.ch>
2022-04-27 17:23:54 +0200

    fcn bugfix

--- src/main.py
+++ src/main.py
@@ -94,27 +94,27 @@ if args.save_path:
 if args.model_to_train == "selfexplainer":
     model = SelfExplainer(
         num_classes=num_classes, dataset=args.dataset, learning_rate=args.learning_rate, pretrained=args.use_imagenet_pretraining, use_weighted_loss=args.use_weighted_loss, 
-        use_similarity_loss=args.use_similarity_loss, use_entropy_loss = args.use_entropy_loss, use_mask_area_loss=args.use_mask_area_loss, save_path=args.save_path, save_masked_images=args.save_masked_images,
+        use_similarity_loss=args.use_similarity_loss, use_entropy_loss = args.use_entropy_loss, use_mask_area_loss=args.use_mask_area_loss, use_mask_variation_loss=args.use_mask_variation_loss, save_path=args.save_path, save_masked_images=args.save_masked_images,
          save_masks=args.save_masks, gpu=args.gpu, profiler=profiler, use_perfect_mask=args.use_perfect_mask, count_logits=args.count_logits
     )
     if args.checkpoint != None:
         model = model.load_from_checkpoint(
             args.checkpoint,
             num_classes=num_classes, dataset=args.dataset, learning_rate=args.learning_rate, pretrained=args.use_imagenet_pretraining, use_weighted_loss=args.use_weighted_loss, 
-        use_similarity_loss=args.use_similarity_loss, use_entropy_loss = args.use_entropy_loss, use_mask_area_loss=args.use_mask_area_loss, save_path=args.save_path, save_masked_images=args.save_masked_images,
+        use_similarity_loss=args.use_similarity_loss, use_entropy_loss = args.use_entropy_loss, use_mask_area_loss=args.use_mask_area_loss, use_mask_variation_loss=args.use_mask_variation_loss, save_path=args.save_path, save_masked_images=args.save_masked_images,
          save_masks=args.save_masks, gpu=args.gpu, profiler=profiler, use_perfect_mask=args.use_perfect_mask, count_logits=args.count_logits
         )
 elif args.model_to_train == "fcn":
     model = FCN16(
         num_classes=num_classes, dataset=args.dataset, learning_rate=args.learning_rate, pretrained=args.use_imagenet_pretraining, use_weighted_loss=args.use_weighted_loss, 
-        use_similarity_loss=args.use_similarity_loss, use_entropy_loss = args.use_entropy_loss, use_mask_area_loss=args.use_mask_area_loss, save_path=args.save_path, save_masked_images=args.save_masked_images,
+        use_similarity_loss=args.use_similarity_loss, use_entropy_loss = args.use_entropy_loss, use_mask_area_loss=args.use_mask_area_loss, use_mask_variation_loss=args.use_mask_variation_loss, save_path=args.save_path, save_masked_images=args.save_masked_images,
          save_masks=args.save_masks, gpu=args.gpu, profiler=profiler, use_perfect_mask=args.use_perfect_mask, count_logits=args.count_logits
     )
     if args.checkpoint != None:
         model = model.load_from_checkpoint(
             args.checkpoint,
             num_classes=num_classes, dataset=args.dataset, learning_rate=args.learning_rate, pretrained=args.use_imagenet_pretraining, use_weighted_loss=args.use_weighted_loss, 
-        use_similarity_loss=args.use_similarity_loss, use_entropy_loss = args.use_entropy_loss, use_mask_area_loss=args.use_mask_area_loss, save_path=args.save_path, save_masked_images=args.save_masked_images,
+        use_similarity_loss=args.use_similarity_loss, use_entropy_loss = args.use_entropy_loss, use_mask_area_loss=args.use_mask_area_loss, use_mask_variation_loss=args.use_mask_variation_loss, save_path=args.save_path, save_masked_images=args.save_masked_images,
          save_masks=args.save_masks, gpu=args.gpu, profiler=profiler, use_perfect_mask=args.use_perfect_mask, count_logits=args.count_logits
         )
 elif args.model_to_train == "classifier":
