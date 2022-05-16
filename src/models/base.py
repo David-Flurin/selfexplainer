2605bc3ec66748446c668055e50f6e01aacecd4a
David <davidn@ethz.ch>
2022-05-16 16:23:59 +0200

    bugfix

--- src/models/base.py
+++ src/models/base.py
@@ -128,7 +128,7 @@ class BaseModel(pl.LightningModule):
 
 
     def setup_metrics(self, num_classes, metrics_threshold):
-        if self.dataset in ['COLOR', 'TOY', 'SMALLVOC', 'VOC']:
+        if self.dataset in ['COLOR', 'TOY', 'TOY_SAVED' 'SMALLVOC', 'VOC']:
             self.train_metrics = ClassificationMultiLabelMetrics(metrics_threshold, num_classes=num_classes, gpu=self.gpu)
             self.valid_metrics = ClassificationMultiLabelMetrics(metrics_threshold, num_classes=num_classes, gpu=self.gpu)
             self.test_metrics = ClassificationMultiLabelMetrics(metrics_threshold, num_classes=num_classes, gpu=self.gpu)
@@ -207,48 +207,6 @@ class BaseModel(pl.LightningModule):
             targets = get_targets_from_segmentations(seg, dataset=self.dataset, num_classes=self.num_classes, gpu=self.gpu, include_background_class=False)
         target_vector = get_targets_from_annotations(annotations, dataset=self.dataset, num_classes=self.num_classes, gpu=self.gpu)
 
-        # bb = image.size()[0]
-        # from matplotlib import pyplot as plt
-        # fig = plt.figure()
-        # for b in range(bb):
-        #     fig.add_subplot(1, bb, b+1)
-        #     plt.imshow(image[b].permute(1,2,0))
-        # plt.show()
-
-        '''
-        for b in range(image.size()[0]):
-            s = image[b].sum().item()
-            if s in self.same_images.keys():
-                self.same_images[s] += 1
-            else:
-                self.same_images[s] = 1
-        '''
-
-        # if self.first_of_epoch:
-        #     from matplotlib import pyplot as plt
-        #     fig = plt.figure()
-        #     batch_size = image.size()[0]
-        #     for b in range(batch_size):
-        #         fig.add_subplot(1, batch_size, b+1)
-        #         plt.imshow(image[b].permute(1,2,0))
-        #     plt.show()
-        #     self.first_of_epoch = False
-        # from matplotlib import pyplot as plt
-        # fig = plt.figure(figsize=(10, 5))
-        # fig.add_subplot(1,3,1)
-        # plt.imshow(image[0])
-        # fig.add_subplot(1,3,2)
-        # plt.imshow(targets[0][0])
-        # fig.add_subplot(1,3,3)
-        # plt.imshow(targets[0][1])
-        # plt.show()
-        # if self.dataset == 'TOY':
-        #     for a in annotations:
-        #         for obj in a['objects']:
-        #             self.shapes_dist.update(obj[0])
-        #             self.f_tex_dist.update(obj[1])
-        #     self.b_text_dist.update(a['background'])
-        
         if self.frozen and self.i % self.freeze_every == 0 and (self.use_similarity_loss or self.use_background_loss):
            self.frozen = deepcopy(self.model)
            for _,p in self.frozen.named_parameters():
@@ -266,32 +224,7 @@ class BaseModel(pl.LightningModule):
         if self.use_background_loss:
             self.test_background_logits.append(output['background'][3].sum().item())
 
-        #GPUtil.showUtilization()
-        # perfect_mask = torch.max(targets, dim=1)[0].unsqueeze(1)
-        # masked_img = image*perfect_mask
-        # inv_masked_img = image*(torch.ones_like(perfect_mask) - perfect_mask)
-        # output1 = self(masked_img, target_vector)
-        # output2 = self(inv_masked_img, target_vector)
-        # # from matplotlib import pyplot as plt
-        # # fig = plt.figure()
-        # # bb = image.size()[0]
-        # # for b in range(image.size()[0]):
-        # #     fig.add_subplot(bb, 5, (b*5)+1)
-        # #     plt.imshow(image[b].permute(1,2,0))
-        # #     fig.add_subplot(bb, 5, (b*5)+2)
-        # #     plt.imshow(perfect_mask[b].permute(1,2,0))
-        # #     fig.add_subplot(bb, 5, (b*5)+3)
-        # #     plt.imshow((torch.ones_like(perfect_mask) - perfect_mask)[b].permute(1,2,0))
-        # #     fig.add_subplot(bb, 5, (b*5)+4)
-        # #     plt.imshow(output1['image'][1][b].detach(), vmin=0, vmax=1)
-        # #     fig.add_subplot(bb, 5, (b*5)+5)
-        # #     plt.imshow(output2['image'][1][b].detach(), vmin=0, vmax=1)
-        # # plt.show()
-        # mask_loss = self.classification_loss_fn(output1['image'][3], target_vector)
-        # inv_mask_loss = bg_loss(output2['image'][0], use_softmax=True)
-        # self.log('mask_loss', mask_loss.item())
-        # self.log('inv_mask_loss', inv_mask_loss.item())
-        # loss =  mask_loss + inv_mask_loss 
+ 
 
 
         if self.objective == 'classification':
@@ -299,18 +232,6 @@ class BaseModel(pl.LightningModule):
             #classification_loss_object = self.classification_loss_fn(o_logits, targets)
             #classification_loss_background = self.classification_loss_fn(b_logits, targets)
         elif self.objective == 'segmentation':
-            # if self.i > 800:
-            #     from matplotlib import pyplot as plt
-            #     fig = plt.figure(figsize=(10, 5))
-            #     fig.add_subplot(2,2,1)
-            #     plt.imshow(output['image'][0][0][0].detach())
-            #     fig.add_subplot(2,2,2)
-            #     plt.imshow(output['image'][0][0][1].detach())
-            #     fig.add_subplot(2,2,3)
-            #     plt.imshow(targets[0][0])
-            #     fig.add_subplot(2,2,4)
-            #     plt.imshow(targets[0][1])
-            #     plt.show()
             targets = targets.to(torch.float64)
             classification_loss_initial = self.classification_loss_fn(output['image'][0], targets)
         else:
@@ -320,19 +241,6 @@ class BaseModel(pl.LightningModule):
 
         classification_loss = classification_loss_initial
         self.log('classification_loss', classification_loss)        
-
-        #if classification_loss.item() > 0.5 and self.i > 20 and self.i % 2 == 0:
-        # b_s,_,_,_ = image.size()
-        # for b in range(2):
-        #     f, axes = plt.subplots(3,8) 
-        #     f.set_size_inches(12,8)
-        #     for s in range(7):
-        #         axes[0][s].imshow(output['image'][0][b][s].detach(), vmin=-5, vmax=5)
-        #         axes[1][s].imshow(output['object'][0][b][s].detach(),vmin=-5, vmax=5)
-        #     axes[0][7].imshow(output['image'][1][b].detach(), vmin=0, vmax=1)
-        #     axes[1][7].imshow(output['object'][1][b].detach(), vmin=0, vmax=1)
-        #     axes[2][0].imshow(image[b].T)
-        # plt.show()
 
         loss = classification_loss
         
