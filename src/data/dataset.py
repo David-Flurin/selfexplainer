c178f4a5731e10e14b81d9236496991a7333b446
David <davidn@ethz.ch>
2022-06-08 17:52:24 +0200

    bugfix

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -108,7 +108,7 @@ class OIDataset(Dataset):
         
         self.images = list(self.labels.keys())
 
-        self.t_class_list = ['Person', 'Cat', 'Dog', 'Bird', 'Cow', 'Horse', 'Sheep', 'Airplane', 'Bicycle', 'Boat', 'Bus', 'Car', 'Motorbike', 'Train', 'Bottle', 'Chair', 'Dining table', 'Potted plant', 'Sofa', 'TV']
+        self.t_class_list = ['Person', 'Cat', 'Dog', 'Bird', 'Cow', 'Horse', 'Sheep', 'Airplane', 'Bicycle', 'Boat', 'Bus', 'Car', 'Motorbike', 'Train', 'Bottle', 'Chair', 'Diningtable', 'Pottedplant', 'Sofa', 'TV']
         for c in self.t_class_list:
             s = sum(c in value for value in self.labels.values())
             print(f'{c}:', s)
