3abfd2db8e46b0818ed880556ea72aeb5122fbf9
David <davidn@ethz.ch>
2022-06-15 13:43:56 +0200

    simloss weighted

--- src/models/base.py
+++ src/models/base.py
@@ -121,10 +121,11 @@ class BaseModel(pl.LightningModule):
 
 
     def setup_losses(self, class_mask_min_area, class_mask_max_area, target_threshold, non_target_threshold):
+        pos_weights = torch.ones(self.num_classes, device=self.device)*self.num_classes * torch.Tensor(get_class_weights(self.dataset), device=self.device)
         if not self.multiclass:
             self.classification_loss_fn = nn.CrossEntropyLoss()
         else:
-            self.classification_loss_fn = nn.BCEWithLogitsLoss(pos_weight=torch.ones(self.num_classes, device=self.device)*self.num_classes * torch.Tensor(get_class_weights(self.dataset), device=self.device))
+            self.classification_loss_fn = nn.BCEWithLogitsLoss(pos_weight=pos_weights)
         # elif self.class_loss == 'threshold':
         #     self.classification_loss_fn = lambda logits, targets: relu_classification(logits, targets, target_threshold, non_target_threshold)
         # else:
@@ -133,7 +134,7 @@ class BaseModel(pl.LightningModule):
         if self.objective == 'segmentation':
             self.classification_loss_fn = nn.BCEWithLogitsLoss()
         
-        self.similarity_loss_fn = nn.CrossEntropyLoss()
+        self.similarity_loss_fn = nn.CrossEntropyLoss(weights = pos_weights)
 
         self.total_variation_conv = TotalVariationConv()
         self.class_mask_area_loss_fn = ClassMaskAreaLoss(min_area=class_mask_min_area, max_area=class_mask_max_area, gpu=self.gpu)
