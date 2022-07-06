a9d9390d0f8b97452d500ad936423356aad3ab07
David <davidn@ethz.ch>
2022-07-06 20:58:33 +0200

    test bugfix and weighting koeffs

--- src/utils/argparser.py
+++ src/utils/argparser.py
@@ -98,6 +98,9 @@ def get_parser():
     parser.add_argument('--use_loss_scheduling', default='False', type=str2bool, help='Scheduling')
     parser.add_argument('--use_mask_logit_loss', default='False', type=str2bool, help='Mask Logit loss')
     parser.add_argument('--mask_logit_loss_regularizer', default='1.0', type=float, help='Mask Logit loss regularizer')
+    parser.add_argument('--object_loss_weighting_params', default='[2, 0.2]', type=float, nargs=2, help='Mask Logit loss regularizer')
+    parser.add_argument('--mask_loss_weighting_params', default='[5, 0.1]', type=float, nargs=2, help='Mask Logit loss regularizer')
+
 
 
     
