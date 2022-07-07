95ead76f574c8eae3a228e7dc9581a497790e3fb
David <davidn@ethz.ch>
2022-07-07 15:42:45 +0200

    background loss bugfix

--- src/models/selfexplainer.py
+++ src/models/selfexplainer.py
@@ -10,10 +10,6 @@ class SelfExplainer(BaseModel):
         super().__init__(**kwargs)
 
         self.model = Deeplabv3Resnet50Model(num_classes=kwargs['num_classes'], pretrained=kwargs['pretrained'], aux_classifier=kwargs['aux_classifier'])
-        if kwargs['aux_classifier']:
-            self.bg_loss = 'entropy'
-        else:
-            self.bg_loss = 'distance'
 
         if kwargs['dataset'] == 'MNIST':
             self.model.model.backbone.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
