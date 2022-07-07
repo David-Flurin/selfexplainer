394f452ce6cc6915f3b4f3e131b082ed6676247a
David <davidn@ethz.ch>
2022-07-07 15:56:58 +0200

    sim loss bugfix

--- src/models/base.py
+++ src/models/base.py
@@ -649,10 +649,10 @@ class BaseModel(pl.LightningModule):
             if self.multiclass:
                 sim_loss = similarity_loss_fn(output, target_vector, self.similarity_loss_fn, self.similarity_regularizer, mode=self.similarity_loss_mode)
             else:
-                logit_fn = torch.sigmoid if self.multiclass else lambda x: torch.nn.functional.softmax(x, dim=-1)
+                logit_fn = lambda x: torch.nn.functional.softmax(x, dim=-1)
                 detached = output['image'][3].detach()
                 probs = logit_fn(detached)
-                sim_loss = self.similarity_regularizer * self.classification_loss_fn(logit_fn(output['object_0'][3]), probs) 
+                sim_loss = self.similarity_regularizer * self.classification_loss_fn(output['object_0'][3]), probs) 
                 
             self.log('val_similarity_loss', sim_loss)
             
