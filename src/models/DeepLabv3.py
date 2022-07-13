356cba04276eca2f5514dfc8f7e1db7317402f86
David <davidn@ethz.ch>
2022-07-13 08:51:05 +0200

    resnet aux head

--- src/models/DeepLabv3.py
+++ src/models/DeepLabv3.py
@@ -7,6 +7,7 @@ from torchvision.models.segmentation.deeplabv3 import DeepLabV3
 from collections import OrderedDict
 from typing import Optional, Dict
 from torch.nn import functional as F
+from copy import deepcopy
 
 class Deeplabv3Resnet50Model(pl.LightningModule):
     def __init__(self, pretrained=False, num_classes=20, aux_classifier=False):
@@ -20,7 +21,7 @@ class Deeplabv3Resnet50Model(pl.LightningModule):
 
         self.aux_classifier = aux_classifier
         if self.aux_classifier:
-            a_c = FCHead_old(2048, num_classes)
+            a_c = FCHead_old(2048, num_classes, deepcopy(self.model.backbone.layer4))
             self.model = ModifiedDeepLab(self.model.backbone, self.model.classifier, a_c)
 
 
@@ -35,12 +36,14 @@ class Deeplabv3Resnet50Model(pl.LightningModule):
 
 
 class FCHead_old(nn.Module):
-    def __init__(self, in_channel, num_classes):
+    def __init__(self, in_channel, num_classes, layer4):
         super().__init__()
+        self.last_resnet_layer = layer4
         self.pool = nn.AdaptiveAvgPool2d((1,1))
         self.fc = nn.Linear(in_channel, num_classes)
 
     def forward(self, x):
+        x = self.last_resnet_layer(x)
         x = self.pool(x)
         x = torch.flatten(x, 1)
         return self.fc(x)
@@ -77,7 +80,7 @@ class ModifiedDeepLab(DeepLabV3):
         result["out"] = x
 
         if self.aux_classifier is not None:
-            x = features["out"]
+            x = features["aux"]
             x = self.aux_classifier(x)
             result["aux"] = x
 
