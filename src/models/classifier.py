552233eafac2d74b73a040b3db0c6cfc5e401c7e
David <davidn@ethz.ch>
2022-07-20 16:09:58 +0200

    fixed explainer to singlelabel

--- src/models/classifier.py
+++ src/models/classifier.py
@@ -177,7 +177,7 @@ class Resnet50ClassifierModel(pl.LightningModule):
         logits = self(x)
         targets = get_targets_from_annotations(y, dataset=self.dataset)
 
-        if self.dataset == "CUB":
+        if dataset in ["TOY", "OI"]:
             labels = targets.argmax(dim=1)
             loss = self.classification_loss_fn(logits, labels)
         else:
